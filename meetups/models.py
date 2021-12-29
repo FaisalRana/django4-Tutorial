a833b1663c94a9fb0ab0a19bec298fe6f179206f
Faisal Rana <inquisitive@gmail.com>
2021-12-28 22:57:37 -0800

    added database and linked it to views and models

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# creates a blueprint for our objects and for our database
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=200) # text is expected in a database table 
+    slug = models.SlugField(unique=True) # verifies that duplicate slugs DNE
+    description = models.TextField()
--- meetups/views.py
+++ meetups/views.py
@@ -3,12 +3,24 @@ from django.http import HttpResponse
 # Create your views here.
 # what happens when the user goes to a url 
 
+from .models import Meetup
+
 def index(request): 
-  meetups = [
-    { 'title': 'A First Meetup'},
-    { 'title': 'A second Meetup'}
-  ]
+  meetups = Meetup.objects.all
+  
+  # meetups = [
+  #   { 'title': 'A First Meetup'},
+  #   { 'title': 'A second Meetup'}
+  # ]
   return render(request, 'meetups/index.html', {
       'meetups': meetups,
-      'show_meetups': False,
+      'show_meetups': True,
+    })
+
+def meetup_details(request):
+    selected_meetup = {'title': 'A first Meetup', 'description': 'This is the first meetup!'}
+    return render(request, 'meetups/meetup-detail.html',{
+      'meetup_title': selected_meetup['title'],
+      'meetup_description': selected_meetup['description']
+
     })
